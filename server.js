0e94dfd10d79875b0dad3f9957e72794226f4691
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-20 14:02:23 -0400

    google-oauth2.0 setup

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const passport = require("passport");
 const PORT = (process.env.PORT || 3000);
+const passportConfig = require("./app/Helpers/passport");
+
+app.get("/auth/google", passport.authenticate("google", {
+	scope: ["profile", "email"]
+}));
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 
 app.listen(PORT, (err) =>{
 	if(err) return console.log("Server error on init: ", err);
