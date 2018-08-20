3a82eba583766a48e50f388355bd709c6ea17a39
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-20 15:01:02 -0400

    refactored codebase

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const passport = require("passport");
 const PORT = (process.env.PORT || 3000);
-const passportConfig = require("./app/Helpers/passport");
 
-app.get("/auth/google", passport.authenticate("google", {
-	scope: ["profile", "email"]
-}));
+// MIDDLEWARES
+require("./app/Config/passport");
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+// ROUTES
+app.use(require("./app/Routes/auth"));
 
-
+// SERVER INIT
 app.listen(PORT, (err) =>{
 	if(err) return console.log("Server error on init: ", err);
 	return console.log(`App is live on port ${PORT}`);
