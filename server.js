24b46dd2a465b6e0022979885b5ac58aa86ba15e
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-21 17:35:21 -0400

    configured client and server to run concurrently

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const PORT = (process.env.PORT || 3000);
+const PORT = (process.env.PORT || 5000);
 
 // DATABASE
 require("./app/Database");
