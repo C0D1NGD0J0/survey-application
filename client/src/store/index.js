3d0c4940574307098d01eccd2f0e857986cd9810
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-22 16:25:47 -0400

    updated proxy rule in react package.json file

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import rootReducer from "../reducers";
 import { applyMiddleware, createStore } from "redux";
+import reduxThunk from "redux-thunk";
 
-const store = createStore(rootReducer, {}, applyMiddleware());
+const store = createStore(rootReducer, {}, applyMiddleware(reduxThunk));
 
 export default store;
