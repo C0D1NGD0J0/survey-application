f1b64e3d598a977bd4594e3ccf5bcf5f9d4210a9
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-29 08:54:15 -0400

    survey create method added

--- app/Controllers/survey.js
+++ app/Controllers/survey.js
@@ -1,8 +1,16 @@
-const mongoose = require('mongoose');
+const Survey = require("../Models/Survey");
 
 const surveyCntrl = {
 	create: (req, res, next) =>{
+		const { title, subject, body, recipients, userResponses } = req.body;
 		
+		const _survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(",").map(email => ({email: email.trim()})),
+			_user: req.user.id;
+		});
 	}
 }
 
